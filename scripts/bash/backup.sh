cc7fe554498e341660351525e3b1d498063851be
Jose Laruta <you@example.com>
2019-07-22 21:17:35 -0400

    test users

---
+++ scripts/bash/backup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# obtener una lista de usuarios
+
+# verificar si existe el directorio backups, si no existe crearlo
+backups_data="/tmp/backups/backups_data"
+
+echo "Verificando que exista el directorio de backups"
+if [ -d "/tmp/backups" ]; then
+    echo "El directorio existe"
+else
+    echo "El directorio NO existe, creando..."
+    mkdir -p /tmp/backups/backups_data
+fi
+
+
+users=$( ls /home )
+
+# para cada usuario en la lista
+
+for user in $users; do
+    echo "========================="
+    echo "Procesando usuario: $user"
+    echo "========================="
+    # verificar el ultimo backup
+    backup_data_file="${backups_data}/${user}_last_backup.log"
+    last_n_files=0
+    last_n_dirs=0
+    if [ -f "$backup_data_file" ]; then
+        echo "el archivo existe!"
+        # leer el contenido
+        content=($( cat $backup_data_file )) #convertir una lista a un array bash
+        last_n_files=${content[0]}
+        last_n_dirs=${content[1]}
+        
+    else
+        echo "El archivo NO existe, creando..."
+        # crear archivos
+        touch $backup_data_file
+        echo "$last_n_files" >> $backup_data_file
+        echo "$last_n_dirs" >> $backup_data_file
+    fi
+    echo "Ultimo backup: $last_n_files archivos y $last_n_dirs carpetas"
+    # obtener numero de archivos actuales
+    user_dir="/home/$user"
+    n_files=$(find $user_dir -type f | wc -l)
+    n_dirs=$(find $user_dir -type d | wc -l)
+    echo "Encontrados $n_files archivos y $n_files carpetas"
+done
+
+
